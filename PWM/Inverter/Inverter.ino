8f78510a03b30ef373c7f604d1bb770e7987ced0
daube <dana.aube17@gmail.com>
2015-11-04 11:31:53 -0500

    Basic PWM changing with Pot

---
+++ PWM/Inverter/Inverter.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  delayMicroseconds(35);
+  digitalWrite(6,LOW);
+  digitalWrite(5,HIGH);
+  delayMicroseconds(5);
+}
--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -1,9 +1,16 @@
+int val = 0;
+
 void setup() {
-  pinMode(3,OUTPUT);
-  TCCR2B = (TCCR2B & 0b11111000) | 0x01;
+  pinMode(3, OUTPUT);  //PWM signal output
+  pinMode(A0, INPUT);  //Input from POT to change PWM signal
+  //pinMode(A1, INPUT);  //Input from Encoder Channel A
+  //pinMode(A2, INPUT);  //Input from Encoder Channel B
+  TCCR2B = (TCCR2B & 0b11111000) | 0x01;  //change frequency to 31.25kHz
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-analogWrite(3,200);
+  val = analogRead(A0)/4;  //Divide by 4 because analogRead is 1024 but write is 256
+  analogWrite(3, val);     //PWM signal (pin#, DutyCycle)
+  
+  
 }
